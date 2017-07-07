8d40f2ae0c6ed63470b2c6a02626cd6a15fbb3e2
Divyansh Jain <devdjain@hotmail.com>
2017-07-08 00:41:11 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,19 @@
+import urllib
+import os
+from bs4 import BeautifulSoup
+url=""
+path=""
+data=urllib.urlopen(url).read()
+soup=BeautifulSoup(data)
+names=[]
+for link in soup.find_all('a',attrs={'class':"pl-video-title-link yt-uix-tile-link yt-uix-sessionlink spf-link "}):
+    names.append(link.contents)
+names=[str(b.strip()) for a in names for b in a]
+names=[a.replace('/','_')for a in names]
+print names
+video_names=[]
+for video in os.listdir(path):
+    video_names.append(video[:-4])
+print video_names
+for id in video_names:
+    os.rename(path+"\\"+id+".mp4",path+"\\"+str(names.index(id))+" "+id+".mp4")
